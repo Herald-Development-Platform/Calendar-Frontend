ab9d57d7599767a9d19695e5a645eadef57a7b9e
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2024-07-02 13:57:20 +0545

    added new backend base url

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -5,13 +5,13 @@ export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:10000/api"
     // : "https://calendar-backend-hbvl.onrender.com/api";
-: "http://localhost:10000/api";
+: "https://calendar-backend-txel.onrender.com/api";
 
 export const webSocketUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:10000"
     // : "https://calendar-backend-hbvl.onrender.com";
-: "http://localhost:10000";
+: "https://calendar-backend-txel.onrender.com";
 
 export const Axios = axios.create({
   baseURL: baseUrl,
