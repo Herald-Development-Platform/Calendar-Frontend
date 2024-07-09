4f50a06c020b78c2113a55900252cbd6b5ef81fb
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2024-07-09 15:50:22 +0545

    build error

--- src/app/(all-pages)/summary/page.tsx
+++ src/app/(all-pages)/summary/page.tsx
@@ -33,6 +33,7 @@ export default function SummaryPage() {
   const [filteredEvents, setFilteredEvents] = useState<any[]>([]);
 
   useEffect(() => {
+    if (!events || events.length === undefined || events.length === 0) return;
     const filteredEvents = events?.filter((e: any) => {
       const date = new Date(e.start);
       const currentDate = new Date();
