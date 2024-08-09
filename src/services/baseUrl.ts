6cfa6c0a3e51ba82660d6c7eb8be19e0f08749e9
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2024-08-09 11:54:42 +0545

    fixed base url

--- src/services/baseUrl.ts
+++ src/services/baseUrl.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+// "https://calendar-backend-txel.onrender.com/api";
 
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://10.99.0.35:10000/api"
-    : // "http://localhost:10000/api"
-      "https://calendar-backend-txel.onrender.com/api";
-// "http://localhost:10000/api";
+    : "http://10.99.0.35:10000/api"
+
 
 export const webSocketUrl =
   process.env.NODE_ENV === "development"
     ? "http://10.99.0.35:10000"
-    : "https://calendar-backend-txel.onrender.com";
-// "http://localhost:10000";
+    : "http://10.99.0.35:10000"
 
 export const Axios = axios.create({
   baseURL: baseUrl,
