02f60750fcb5014f2922338148a09674a117aaf6
sammy-boii <neupanesamrajya2005@gmail.com>
2024-12-10 13:19:52 +0545

    a

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -161,7 +161,8 @@ export default function Page() {
               className="btn w-full rounded-[4px] bg-primary-50  text-sm  hover:bg-primary-100"
               type="button"
             >
-              <img
+              <Image
+                unoptimized
                 src={"/images/LoginPage/GoogleIcon.png"}
                 width={24}
                 height={24}
