1cb77c61a340c076e37d71c95ea796d4771c7882
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2025-01-02 13:56:18 +0545

    domain prefix after oauth

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -26,7 +26,7 @@ function Oauth() {
       Cookies.set("token", token, {
         expires: 60
       });
-      router.push("/");
+      router.push(`${process.env.NEXT_PUBLIC_DOMAIN_PREFIX}`);
     }
   }, [token]);
   return (
