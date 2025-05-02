4b89462710da21db65ade5e82c793df03e17c181
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-05-02 16:23:37 +0545

    fix: build error due to typings

--- src/app/(all-pages)/layout.tsx
+++ src/app/(all-pages)/layout.tsx
@@ -1,16 +1,15 @@
+import React from "react";
 import Sidebar from "@/components/Sidebar";
 import { Suspense } from "react";
 
 export default function RootLayout({
   children,
-  searchParams,
 }: {
   children: React.ReactNode;
-  searchParams: { show_procurement: boolean };
 }) {
   return (
     <div className="flex">
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <Sidebar hasBreakpoint={true}></Sidebar>
         <div className="flex h-screen w-full flex-col">{children}</div>
       </Suspense>
